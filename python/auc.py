22c8f0c2651cb4a21f38a453e68e5b631cbe0902
Kirill Pavlov <pavlov99@yandex.ru>
2011-02-28 01:42:11 +0300

    Add vif calculation

--- python/auc.py
+++ python/auc.py
@@ -72,7 +72,7 @@ def least_squares(x, y):
     if y.shape[0] == 1:
         y = y.T
 
-    return (x.T * x)**(-1) * x.T * y
+    return np.asarray((x.T * x)**(-1) * x.T * y)
     
     
 def regression_residuals(x, y):
@@ -89,7 +89,7 @@ def regression_residuals(x, y):
     if y.shape[0] == 1:
         y = y.T
     
-    return y - x * least_squares(x, y)
+    return np.asarray(y - x * np.asmatrix(least_squares(x, y)))
     
     
 def get_vif(x):
@@ -98,12 +98,14 @@ def get_vif(x):
     """
     x = np.asarray(x)
     (numer_objects, number_features) = x.shape
-    
     vif = np.empty([number_features, 1])
-    print vif
     
-    pass
+    for i in xrange(number_features):
+        rows =  range(i) + range(i + 1, number_features)
+        vif[i] = sum((x[:, i] - np.mean(x[:, i]))** 2) /\
+        sum(regression_residuals(x[:, rows], x[:, i]) ** 2)
 
+    return vif
 
 def get_belsley(x):
     """
@@ -118,10 +120,11 @@ if __name__ == "__main__":
     doctest.testmod()
     
     print 'Done'
-    x = np.array([[1, 2], [5, 7], [12, 1]])
+    x = np.array([[1, 2, 4], [5, 7, 2], [12, 1, 7]])
 
     # print regression_residuals(x, np.array([[1, 1, 2]]))
-    # print get_vif(x)
+    print get_vif(x)
+    # print x - np.tile(np.mean(x, axis=0), (3, 1))
     
     
     
