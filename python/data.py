bad8fdffbfbb45fa012f511bf836d7cd30cdf0cf
Kirill Pavlov <pavlov99@yandex.ru>
2011-03-01 02:55:37 +0300

    add vif calculation as method of data class

--- python/data.py
+++ python/data.py
@@ -6,7 +6,26 @@ class ObjectFeatureMatrix(object):
         self.__number_objects, self.__number_features = self.__objects.shape
     
     def vif(self):
-        pass
+        def _regression_residuals(x, y):
+            """
+            Calculate regression residuals:
+            y - x * (x' * x)^(-1) * x' * y;
+            Input:
+                x - array(l, n)
+                y - array(l, 1)
+            Output:
+                residuals y - x*w - array(l, 1)
+            """
+            x, y = np.asmatrix(x), np.asmatrix(y).T
+            return np.asarray(y - x * (x.T * x)**(-1) * x.T * y)
+        
+        vif = np.empty([self.__number_features, 1])
+        for i in xrange(self.__number_features):
+            rows = range(i) + range(i + 1, self.__number_features)
+            vif[i] = sum((self.__objects[:, i] - np.mean(self.__objects[:, i]))** 2) /\
+            sum(_regression_residuals(self.__objects[:, rows], self.__objects[:, i]) ** 2)
+
+        return vif
         
     def get_number_objects(self): 
         return self.__number_objects
@@ -21,5 +40,5 @@ class ObjectFeatureMatrix(object):
 
 x = [[1, 2], [5, 7], [12, 1]]
 d = ObjectFeatureMatrix(x)
-# print d._ObjectFeatureMatrix__objects
-# print d._number_objects
+print d.vif()
+print "\n".join(dir(d))
