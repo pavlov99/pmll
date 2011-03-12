f8762d9973a5401129ebc30ddb9cb0a42c187e94
Kirill Pavlov <pavlov99@yandex.ru>
2011-03-12 11:22:30 +0300

    Change data: move objects from privat to public field

--- python/data.py
+++ python/data.py
@@ -1,20 +1,32 @@
 import numpy as np
 
 class ObjectFeatureMatrix(object):
-    def __init__(self, matrix):
+    def __init__(self, matrix=None):
+        self.objects = None
+        self.nobjects = 0 
+        self.nfeatures = None
+
+        if matrix is not None:
+            self.add(matrix)
+
+    def __str__(self):
+        print self.objects
+
+    def add(self, matrix):
         matrix = np.asmatrix(matrix)
-        self.__objects = matrix
-        self.__number_objects, self.__number_features = self.__objects.shape
-        
-    def add_data(self, matrix):
-        matrix = np.asmatrix(matrix)
-        if matrix.shape[1] != self.__number_features:
-            raise AssertionError("Additional and current objects has different \
-                numbers of features")
-        self.__objects = np.vstack([self.__objects, matrix])
-        self.__number_objects += matrix.shape[0]
-    
+        if self.nfeatures is not None:
+            if matrix.shape[1] != self.nfeatures:
+                raise AssertionError("Additional and current objects has different numbers of features")
+            self.objects = np.vstack([self.objects, matrix])
+            self.nobjects += matrix.shape[0]
+        else: # first add
+            self.objects = matrix 
+            self.nobjects, self.nfeatures = self.objects.shape
+
     def vif(self):
+        if self.objects is None:
+            raise ValueError("Can`t use this method for empty data")
+
         def _regression_residuals(x, y):
             """
             Calculate regression residuals:
@@ -26,19 +38,22 @@ class ObjectFeatureMatrix(object):
                 residuals y - x*w - array(l, 1)
             """
             return np.asarray(y - x * (x.T * x)**(-1) * x.T * y)
-        
-        vif = np.empty([self.__number_features, 1])
-        for i in xrange(self.__number_features):
-            rows = range(i) + range(i + 1, self.__number_features)
-            vif[i] = sum(np.asarray(self.__objects[:, i] - np.mean(self.__objects[:, i])) ** 2) /\
-            sum(_regression_residuals(self.__objects[:, rows], self.__objects[:, i]) ** 2)
+
+        vif = np.empty([self.nfeatures, 1])
+        for i in xrange(self.nfeatures):
+            rows = range(i) + range(i + 1, self.nfeatures)
+            vif[i] = sum(np.asarray(self.objects[:, i] - np.mean(self.objects[:, i])) ** 2) /\
+            sum(_regression_residuals(self.objects[:, rows], self.objects[:, i]) ** 2)
 
         return vif
 
     def belsley(self):
+        if self.objects is None:
+            raise ValueError("Can`t use this method for empty data")
+
         from scipy.linalg import svd
 
-        [U,S,V] = svd(self.__objects);
+        [U,S,V] = svd(self.objects);
 
         # divide each row of V.^2 by S(i).^2 - singular value
         Q = np.dot(np.diag((1 / S) ** 2), V ** 2)
@@ -49,28 +64,38 @@ class ObjectFeatureMatrix(object):
         conditionality_indexes = max(S) / S[:,np.newaxis]
         return(conditionality_indexes, Q)
 
-    def get_number_objects(self): 
-        return self.__number_objects
-    nobjects = property(get_number_objects, doc="return number of objects")
-    
-    def get_number_features(self): 
-        return self.__number_features
-    nfeatures = property(get_number_features, doc="return number of features")
-
 
 class DataSet(object):
-    def __init__(self, objects, labels):
+    def __init__(self, objects=None, labels=None):        
+        if (objects is None) ^ (labels is None):
+            raise TypeError("Require zero or two arguments for input")
+
+        self.objects = ObjectFeatureMatrix()
+        self.labels = None
+
+        if objects is not None: # so labels is not None too
+            self.add(objects, labels)
+
+    def add(self, objects, labels):
         objects, labels = np.asmatrix(objects), np.asmatrix(labels)
 
         if labels.shape[0] == 1:
             labels = labels.T
-        
+
         if labels.shape[1] != 1:
             raise AssertionError("One of the label dimensions must be 1")
-        
+
         if labels.shape[0] != objects.shape[0]:
             raise AssertionError("Number of objects must be equal number of labels")
-        
-        self.labels = labels
-        self.objects = ObjectFeatureMatrix(objects)
-        
+
+        self.labels = np.vstack([self.labels, labels])
+        self.objects.add(objects)
+
+    def get_number_objects(self): 
+        return self.objects.nobjects
+    nobjects = property(get_number_objects, doc="return number of objects")
+
+    def get_number_features(self): 
+        return self.objects.nfeatures
+    nfeatures = property(get_number_features, doc="return number of features")
+
