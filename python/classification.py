2951192545cbbcf72843f1d6bcf879f586b21b9e
Kirill Pavlov <pavlov99@yandex.ru>
2011-04-08 23:08:51 +0400

    move doctests for classification to file classification.test

--- python/classification.py
+++ python/classification.py
@@ -43,37 +43,27 @@ class BaseBinaryClassifier(BaseClassifier):
         probabilities = np.asmatrix(self.classify(data.objects))
         return float(sum(labels != (probabilities > self.cut_off))) / len(labels)
 
-    def get_auc(self, labels, probabilities):
+    def get_auc(self, data):
         """
         Calculate Area Under Curve for classification.
     
         Arguments:
-        - `labels`: class labels in {0, 1}
-        - `probabilities`: probabilities of "1" class
+        - `data`: DataSet object
     
         Example:
             size = 100000
-            labels = numpy.asarray(np.random.randint(0, 2, size))
-            import random
-            probabilities = [random.random() for i in xrange(size)]
-
-            auc, fpr, tpr = get_auc(labels, probabilities)
+            zeros = np.zeros([size, 1])
+            data = DataSet(objects=zeros, labels=zeros)
+            
+            auc, fpr, tpr = self.get_auc(data)
             import pylab as p
             p.plot(fpr, tpr)
             p.show()
-
-        >>> get_auc([-1, 1], [0, 0])
-        Traceback (most recent call last):
-            ...
-        ValueError: labels contains not only {0, 1}
-        >>> get_auc([0, 1], [0, 0, 1])
-        Traceback (most recent call last):
-            ...
-        AssertionError: lists has different lengths
-        >>> get_auc([0, 0, 1, 1], [0.0, 0.6, 0.4, 0.8])
-        (0.75, [0, 0, 0.5, 0.5, 1.0], [0, 0.5, 0.5, 1.0, 1.0])
         """
-        labels, probabilities = np.asarray(labels), np.asarray(probabilities)
+
+        labels = np.asarray(data.labels.flatten())[0]
+        probabilities = np.asarray(self.classify(data.objects))
+        
         number_positive = sum(labels == 1)
         number_negative = sum(labels == 0)
     
@@ -129,6 +119,10 @@ class RandomBinaryClassifier(BaseBinaryClassifier):
         predictions = predictions.reshape(-1, 1)
         return predictions
 
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("classification.test")
+    # doctest.testfile("classification.test", verbose=True)
 
 
 
