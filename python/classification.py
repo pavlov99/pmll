d4ac924395db13266cd8be7a57b4ad4891f82d9d
Kirill Pavlov <pavlov99@yandex.ru>
2011-09-05 14:09:41 +0400

    add EmIrls algorithm

--- python/classification.py
+++ python/classification.py
@@ -123,11 +123,13 @@ class RandomBinaryClassifier(BaseBinaryClassifier):
 
 
 class Irls(object):
-    def __init__(self, objects, labels):
+    def __init__(self, objects, labels, weights=None):
+        # 'weights' are used for 1) init weights 2) create model
         objects, labels = np.asmatrix(objects), np.asmatrix(labels)
         self.__objects = np.hstack([objects, np.ones([objects.shape[0], 1])])
         self.__labels = labels
-        self.__weights = np.zeros([self.__objects.shape[1], 1])
+        self.__weights = weights if weights is not None \
+            else np.random.randn(self.__objects.shape[1], 1)
         self.__history = {'weights': self.__weights}
 
     def get_weights(self): return self.__weights
@@ -135,11 +137,15 @@ class Irls(object):
     def __logit(self, z): return 1 / (1 + np.exp(-z))
 
     def classify(self, objects):
-        # convert input to matrix, append constant column and get logit
-        return self.__logit(np.asmatrix(np.hstack([objects,
-                                                   np.ones([objects.shape[0], 1]
-                                                           )])
-                                        ) * self.__weights)
+        #  return logit function of linear regression
+        return self.__logit(self.get_linear_regression(objects))
+
+    def get_linear_regression(self, objects):
+        # convert input to matrix, append constant column
+        # return linear regression
+        return np.asmatrix(np.hstack([objects,
+                                      np.ones([objects.shape[0], 1])
+                                      ])) * self.__weights
 
     def train(self, max_iterations=100, accuracy=1e-7,
               regularization_parameter=1e-7):
@@ -161,6 +167,7 @@ class Irls(object):
 
     def plot_convergence(self):
         from matplotlib import pyplot as plt
+
         plt.plot(self.__history['weights'].T)
         plt.show()
 
@@ -171,15 +178,49 @@ class Irls(object):
 class EmIrls(object):
     def __init__(self, objects, labels, number_models):
         objects, labels = np.asmatrix(objects), np.asmatrix(labels)
-        self.__objects = np.hstack([objects, np.ones([objects.shape[0], 1])])
+        self.__objects = objects
         self.__labels = labels
+        self.__object_model = np.random.randint(0, number_models,
+                                                [self.__objects.shape[0], 1])
+        self.__weights = np.empty([self.__objects.shape[1] + 1, number_models])
         self.__number_models = number_models
 
-    def train(self):
-        pass
+    def __str__(self):
+        return str(self.__weights) + str(sum(self.__object_model==0))
 
-    def classify(self):
-        pass
+    def train(self):
+        for iteration in range(10):
+            print iteration
+
+            # M-step
+            for model_index in range(self.__number_models):
+                indeces = np.asarray((self.__object_model == model_index).nonzero()[0].flat)
+                algo = Irls(self.__objects[indeces, :], self.__labels[indeces])
+                algo.train()
+                self.__weights[:, model_index] = np.asarray(algo.get_weights().flat)
+
+                # E-step
+                # Choose models for objects with gives minimal value to |x * w|
+                algo = Irls(None, None, self.__weights)
+                self.__object_model = abs(algo.get_linear_regression(self.__objects)).argmin(1)
+
+    def classify(self, objects):
+        objects = np.asmatrix(objects)
+        labels = np.empty([objects.shape[0], 1])
+
+        # Choose models for objects with gives minimal value to |x * w|
+        algo = Irls(None, None, self.__weights)
+        object_model = abs(algo.get_linear_regression(self.__objects)).argmin(1)
+
+        # Classify objects
+        for model_index in range(self.__number_models):
+            algo = Irls(None, None, self.__weights[:, model_index][:,np.newaxis])
+            indeces = np.asarray((object_model == model_index).nonzero()[0].flat)
+            labels[indeces] = algo.classify(objects[indeces, :])
+
+        return labels
+
+    def get_weights(self): return self.__weights
 
 
 if __name__ == "__main__":
