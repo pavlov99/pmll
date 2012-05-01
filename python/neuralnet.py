e130690da54f4c83284656a03c83b6d2640eb254
Kirill Pavlov <kirill.pavlov@phystech.edu>
2012-05-02 01:50:50 +0800

    Neural net uncommited data

--- python/neuralnet.py
+++ python/neuralnet.py
@@ -3,7 +3,9 @@
 
 import numpy as np
 import scipy
+from scipy import optimize
 from scipy.io import loadmat
+import itertools
 
 class NeuralLayer(object):
     """
@@ -31,7 +33,7 @@ class NeuralLayer(object):
                                                          self.number_outputs,
                                                          )
                                      )
-            self.weights = initial_weights 
+            self.weights = initial_weights
         else:
             self.weights = self.__init_weights(number_inputs, number_outputs)
 
@@ -43,13 +45,14 @@ class NeuralLayer(object):
     def push_forward(self, objects):
         """
         return output signal of layer given input signal.
-        oblects = (number_objects, number_inputs) matrix
+        objects = (number_objects, number_inputs) matrix
         """
         if self.constant_neuron:
-            return np.hstack([objects, np.ones([objects.shape[0], 1])]) *\
-                self.weights
+            return dot(np.hstack([objects, np.ones([objects.shape[0], 1])]),
+                       self.weights)
         else:
-            return objects * self.weights
+            return np.dot(objects, self.weights)
+
 
 class NeuralNet(object):
     @classmethod
@@ -65,9 +68,21 @@ class NeuralNet(object):
         else:
             return cls.activation_function(z, prime - 1) * \
                 (1 - cls.activation_function(z, prime - 1))
-    
-    
-    def __init__(self, number_neurons):
+
+    def quality_function(self, predictions, labels, regularization=0):
+        """
+        Quality function of neural net.
+        J = -(y==k)' * log(output[:,k]) - (y!=k)' * log(1 - output[:,k])
+        """
+        quality = 0
+        for index, label in enumerate(set(labels.flat)):
+            quality = quality -\
+                np.dot((labels == label).T, np.log(predictions[:, index])) -\
+                np.dot((labels != label).T, np.log(1 - predictions[:, index]))
+        quality = quality / labels.shape[0]
+        return quality
+
+    def __init__(self, number_neurons, constant_neuron=False):
         """
         data - structure with "objects" and "labels" fields
         numberNeurons - number neurons in corresponding layer
@@ -75,25 +90,107 @@ class NeuralNet(object):
         if len(number_neurons) < 2:
             raise AssertionError("Number of layers must be larger or equal to 2. Input layer and output layer.")
 
-        self.layers = [NeuralLayer(number_inputs, number_outputs) 
-                       for number_inputs, number_outputs 
+        self.layers = [NeuralLayer(number_inputs, number_outputs,
+                                   constant_neuron=constant_neuron)
+                       for number_inputs, number_outputs
                        in zip(number_neurons[:-1], number_neurons[1:])]
 
     def push_forward(self, objects):
-        pass
+        output = objects
+        for layer in self.layers:
+            output = self.activation_function(layer.push_forward(output))
+            yield output
+
+    @staticmethod
+    def __normalize_output(matrix):
+        """
+        divide each row of matrix by sum of elements in this row
+        """
+        return matrix / np.tile(np.sum(matrix, 1)[:,np.newaxis],
+                                (1, matrix.shape[1]))
+
+    def get_quality(self, objects, labels, weights=None):
+        """
+        Return quality of neural net. Use quality function for classification.
+        """
+        if weights is not None:
+            initial_weights = self.unroll_weights()
+            self.roll_weights(weights)
+
+        # Neural net output
+        outputs = self.__normalize_output(list(self.push_forward(objects))[-1])
+
+        if weights is not None:
+            self.roll_weights(initial_weights)
+
+        return self.quality_function(outputs, labels)
+
+    def unroll_weights(self):
+        """
+        Return generator - vector of weights
+        """
+        return itertools.chain(*(layer.weights.flat for layer in self.layers))
+
+
+    def roll_weights(self, weights):
+        """
+        Convert vector representation to weights and assign to layers
+        """
+        weights = list(weights)
+        layer_sizes = [layer.weights.size for layer in self.layers]
+        assert(len(weights) == sum(layer_sizes))
+
+        layer_indexes = np.cumsum([0] + layer_sizes)
+        for index, layer in enumerate(self.layers):
+            new_weights = np.asarray(weights[layer_indexes[index]:\
+                                                 layer_indexes[index + 1]])
+            self.layers[index].weights = new_weights.reshape(layer.weights.shape)
+
+
+    def learn_gradient(self, objects, labels, initial_weights=None):
+        weights = initial_weights or list(self.unroll_weights())
+        f = lambda w: self.get_quality(objects, labels, weights=w)
+        weights_opt = optimize.fmin_bfgs(f, weights, maxiter=50, disp=False)
+        self.roll_weights(weights_opt)
 
     def train(objects, labels):
+        """
+        Train neural net using back propogation
+        """
         pass
 
+    def classify(self, objects):
+        return list(self.push_forward(objects))[-1].argmax(1)
+
     @classmethod
-    def from_objects_labels(cls, objects, labels, number_hidden_neurons=[]):
-        return cls([objects.shape[1]] + number_hidden_neurons + [labels.shape[1]])
+    def from_objects_labels(cls, objects, labels, number_hidden_neurons=[],
+                            constant_neuron=False):
+        number_inputs = objects.shape[1]
+        if labels.shape[1] == 1:
+            # labels = list of class labels
+            number_outputs = len(set(labels.flat))
+        else:
+            # labels = matrix, each row is boolean indicator of class.
+            # Line contains 1 in the column corresponding to the object class.
+            number_outputs = labels.shape[1]
+        return cls([number_inputs] + number_hidden_neurons + [number_outputs])
 
 
 
 if __name__ == '__main__':
     data = scipy.io.loadmat('../data/iris.mat')
-    x = data['X'][50:]
-    y = data['Y'][50:] - 1
-    
-    print 'hello'
+    x = data['X'][:]
+    y = data['Y'][:]
+    nn = NeuralNet.from_objects_labels(x, y)
+
+    # print list(nn.unroll_weights())
+    #print nn.get_quality(x, y)
+
+    # print nn.roll_weights(range(12))
+    # print list(nn.unroll_weights())
+    #print nn.get_quality(x, y, range(12))
+
+    # print nn._get_weight_gradient(x, y)
+    nn.learn_gradient(x, y)
+    # print list(nn.push_forward(x))[-1]
+    print nn.classify(x)
