c1a142d0d95aa71ab5de292b8513bb90842338d5
Kirill Pavlov <kp@multichannel.net>
2012-05-06 16:08:43 +0800

    write base classifier method

--- python/pmll/classification.py
+++ python/pmll/classification.py
@@ -8,8 +8,21 @@ class LinearRegressionLeastSquaresModel(object):
     def train(self, objects):
         pass
 
+    @staticmethod
+    def get_weights(objects, labels):
+        """
+        w = (X' * X) ^ {-1} * X' * y
+        """
+        pass
 
-class LinearRegressionLeastSquares(object):
+    def get_regression_residuals(objects, labels):
+        """
+        y - Xw = (I - X * (X' * X) ^ {-1} * X') * y
+        """
+        pass
+
+
+class LinearRegression(object):
     @staticmethod
     def get_regression(objects, weights):
         """
@@ -31,6 +44,7 @@ class LinearRegressionLeastSquares(object):
         Output:
             [X, 1] * w
        """
+        objects = np.asmatrix(objects)
         modified_objects = np.column_stack((
                 objects,
                 np.ones([objects.shape[0], 1]),
@@ -52,8 +66,16 @@ class IrlsModel(object):
     def __logit(z):
         return 1 / (1 + np.exp(-z))
 
-    def train(self):
+    def __init_weights(self, size):
         pass
+
+    def train(self, objects, labels, max_iterations=100, accuracy=1e-5,
+              regularization_parameter=1e-5):
+        objects = np.hstack([
+                objects,
+                np.ones([objects.shape[0], 1]),
+                ])
+        labels = np.asmatrix(labels)
 
 
 class IrlsClassifier(object):
@@ -63,24 +85,27 @@ class IrlsClassifier(object):
     def __init__(self, model):
         self.model = model
 
+    def classify(self, objects):
+        return LinearRegression.get_regression1(objects, self.model.weights)
+
 
 class TestLinearRegressionLeastSquaresModel(unittest.TestCase):
     def setUp(self):
         pass
 
 
-class TestLinearRegressionLeastSquares(unittest.TestCase):
+class TestLinearRegression(unittest.TestCase):
     def test_get_regression1_array_array(self):
         objects = np.random.rand(3,2)
         weights = np.random.rand(3,1)
-        output = LinearRegressionLeastSquares.get_regression1(objects, weights)
+        output = LinearRegression.get_regression1(objects, weights)
         self.assertEqual(output.shape, (3, 1))
         self.assertIsInstance(output, np.matrix)
 
     def test_get_regression1_array_matrix(self):
         objects = np.random.rand(3,2)
         weights = np.matrix(np.random.rand(3,1))
-        output = LinearRegressionLeastSquares.get_regression1(objects, weights)
+        output = LinearRegression.get_regression1(objects, weights)
         self.assertEqual(output.shape, (3, 1))
         self.assertIsInstance(output, np.matrix)
 
@@ -88,20 +113,20 @@ class TestLinearRegressionLeastSquares(unittest.TestCase):
         objects = np.random.rand(3,2)
         # list of arrays
         weights = list(np.random.rand(3,1))
-        output = LinearRegressionLeastSquares.get_regression1(objects, weights)
+        output = LinearRegression.get_regression1(objects, weights)
         self.assertEqual(output.shape, (3, 1))
         self.assertIsInstance(output, np.matrix)
 
         # list. Note, that it has to be list of lists: [[1], [2]] = column
         weights = [[float(w)] for w in weights]
-        output = LinearRegressionLeastSquares.get_regression1(objects, weights)
+        output = LinearRegression.get_regression1(objects, weights)
         self.assertEqual(output.shape, (3, 1))
         self.assertIsInstance(output, np.matrix)
 
     def test_get_regression1_matrix_array(self):
         objects = np.matrix(np.random.rand(3,2))
         weights = np.random.rand(3,1)
-        output = LinearRegressionLeastSquares.get_regression1(objects, weights)
+        output = LinearRegression.get_regression1(objects, weights)
         self.assertEqual(output.shape, (3, 1))
         self.assertIsInstance(output, np.matrix)
 
@@ -113,8 +138,18 @@ class TestIrlsModel(unittest.TestCase):
 
 
 class TestIrlsClassifier(unittest.TestCase):
-    pass
+    def setUp(self):
+        pass
 
+    def test_classify_existing_model(self):
+        objects = [[0], [1]]
+        weights = [[1], [2]]
+        predicted_labels = np.matrix([[2], [3]])
+        model = IrlsModel(weights)
+        classifier = IrlsClassifier(model)
+        labels = classifier.classify(objects)
+        self.assertIsInstance(labels, np.matrix)
+        self.assertEqual(labels.all(), predicted_labels.all())
 
 if __name__ == '__main__':
     unittest.main()
