341cac86b51f627dca6e828c18d2fceb3f3f2786
Kirill Pavlov <kirill.pavlov@phystech.edu>
2012-06-19 23:00:22 +0800

    add info about author

--- python/setup.py
+++ python/setup.py
@@ -3,4 +3,10 @@ setup(
     name="pmll",
     version="0.1",
     packages=find_packages(),
+
+    # metadata for upload to PyPI
+    author="Kirill Pavlov",
+    author_email="kirill.pavlov@phystech.edu",
+    description="Python machine learning library",
+    license="MIT",
 )
