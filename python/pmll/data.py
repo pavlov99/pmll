bbeb08e57f570a922790dc73d39b30eb83fb3da4
Kirill Pavlov <kirill.pavlov@phystech.edu>
2012-10-06 17:50:29 +0800

    update ObjectFeatureMatrix class, add Data and Big Data lasses

--- python/pmll/data.py
+++ python/pmll/data.py
@@ -1,37 +1,42 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 import scipy
 from scipy.io import loadmat
 from collections import namedtuple
 import itertools
+import tempfile
+import settings
+import urllib2
+import gzip
+from StringIO import StringIO
+from scipy.linalg import svd
+import random
+import unittest
 
-class ObjectFeatureMatrix(object):
+__author__  = "Kirill Pavlov"
+__email__   = "kirill.pavlov@phystech.edu"
+
+
+class ObjectFeatureMatrix(np.matrix):
     """
     Class for representation of objects-by-features matrix of features.
     """
-    def __init__(self, matrix=None):
-        self.objects = None
-        self.nobjects = 0
-        self.nfeatures = None
+    @property
+    def nobjects(self):
+        return self.shape[0]
 
-        if matrix is not None:
-            self.add(matrix)
+    @property
+    def nfeatures(self):
+        return self.shape[1]
 
-    def add(self, matrix):
-        matrix = np.asmatrix(matrix)
-        if self.nfeatures is not None:
-            if matrix.shape[1] != self.nfeatures:
-                raise AssertionError("additional and current objects has different numbers of features")
-            self.objects = np.vstack([self.objects, matrix])
-            self.nobjects += matrix.shape[0]
-        else: # first add
-            self.objects = matrix
-            self.nobjects, self.nfeatures = self.objects.shape
-
+    @property
     def vif(self):
-        if self.objects is None:
-            raise ValueError("Can`t use this method for empty data")
-
-        def _regression_residuals(x, y):
+        """
+        Calculate variance inflation factor
+        """
+        if (self.nfeatures < 2):
+            raise ValueError("Objects should have at least 2 features")
+        def __regression_residuals(x, y):
             """
             Calculate regression residuals:
             y - x * (x' * x)^(-1) * x' * y;
@@ -39,25 +44,22 @@ class ObjectFeatureMatrix(object):
                 x - array(l, n)
                 y - array(l, 1)
             Output:
-                residuals y - x*w - array(l, 1)
+                residuals y - x*w : array(l, 1)
             """
             return np.asarray(y - x * (x.T * x)**(-1) * x.T * y)
 
-        vif = np.empty([self.nfeatures, 1])
-        for i in xrange(self.nfeatures):
+        vif = []
+        for i in range(self.nfeatures):
             rows = range(i) + range(i + 1, self.nfeatures)
-            vif[i] = sum(np.asarray(self.objects[:, i] - np.mean(self.objects[:, i])) ** 2) /\
-            sum(_regression_residuals(self.objects[:, rows], self.objects[:, i]) ** 2)
+            residuals = __regression_residuals(self[:, rows], self[:, i])
+            v = self[:, i].std() ** 2 / sum(residuals ** 2)
+            vif.append(float(v))
 
         return vif
 
+    @property
     def belsley(self):
-        if self.objects is None:
-            raise ValueError("Can`t use this method for empty data")
-
-        from scipy.linalg import svd
-
-        [U,S,V] = svd(self.objects);
+        [U,S,V] = svd(self);
 
         # divide each row of V.^2 by S(i).^2 - singular value
         Q = np.dot(np.diag((1 / S) ** 2), V ** 2)
@@ -68,208 +70,44 @@ class ObjectFeatureMatrix(object):
         conditionality_indexes = max(S) / S[:,np.newaxis]
         return(conditionality_indexes, Q)
 
-    def plot(self, features=(0, 1)):
-        """
-        Plot objects in 2D plane (f1, f2). features = (f1, f2)
-        """
-        import matplotlib.pyplot as plt
-        from matplotlib.ticker import NullFormatter
 
-        x, y = self.objects[:, features[0]].flatten().tolist()[0], \
-            self.objects[:, features[1]].flatten().tolist()[0]
-        nullfmt = NullFormatter() # no labels
-
-        # definitions for the axes
-        left, width = 0.1, 0.65
-        bottom, height = 0.1, 0.65
-        bottom_h = left_h = left + width + 0.02
-
-        rect_scatter = [left, bottom, width, height]
-        rect_histx = [left, bottom_h, width, 0.2]
-        rect_histy = [left_h, bottom, 0.2, height]
-
-        # start with a rectangular Figure
-        plt.figure(1, figsize=(8, 8))
-
-        axScatter = plt.axes(rect_scatter)
-        axHistx = plt.axes(rect_histx)
-        axHisty = plt.axes(rect_histy)
-
-        # no labels
-        axHistx.xaxis.set_major_formatter(nullfmt)
-        axHisty.yaxis.set_major_formatter(nullfmt)
-
-        # the scatter plot:
-        axScatter.scatter(x, y)
-
-        # now determine nice limits by hand:
-        binwidth = 0.25
-        xymax = np.max( [np.max(np.fabs(x)), np.max(np.fabs(y))] )
-        lim = ( int(xymax/binwidth) + 1) * binwidth
-
-        axScatter.set_xlim((min(x), max(x)))
-        axScatter.set_ylim((min(y), max(y)))
-
-        bins = np.arange(-lim, lim + binwidth, binwidth)
-        axHistx.hist(x, bins=bins)
-        axHisty.hist(y, bins=bins, orientation='horizontal')
-
-        axHistx.set_xlim( axScatter.get_xlim() )
-        axHisty.set_ylim( axScatter.get_ylim() )
-
-        plt.show()
-
-
-class DataSet(object):
-    def __init__(self, objects=None, labels=None):
-        if (objects is None) ^ (labels is None):
-            raise TypeError("Require zero or two arguments for input")
-
-        self.objects = ObjectFeatureMatrix()
-        self.labels = None
-
-        if objects is not None: # so labels is not None too
-            self.add(objects, labels)
-
-    def add(self, objects, labels):
-        objects, labels = np.asmatrix(objects), np.asmatrix(labels)
-
-        if labels.shape[0] == 1:
-            labels = labels.T
-
-        if labels.shape[1] != 1:
-            raise AssertionError("One of the label dimensions must be 1")
-
-        if labels.shape[0] != objects.shape[0]:
-            raise AssertionError("Number of objects must be equal number of labels")
-
-        if self.labels is None:
-            self.labels = labels
-        else:
-            self.labels = np.vstack([self.labels, labels])
-        self.objects.add(objects)
-
-    def get_number_objects(self):
-        return self.objects.nobjects
-    nobjects = property(get_number_objects, doc="return number of objects")
-
-    def get_number_features(self):
-        return self.objects.nfeatures
-    nfeatures = property(get_number_features, doc="return number of features")
-
-    def plot(self, features=(0, 1)):
-        """
-        Plot data in 'features' axis.
-        FIXIT: add more than 8 colors, specified by COLORS
-        """
-        import matplotlib.pyplot as plt
-        COLORS="rbgcmykw"
-
-        labels = np.asarray(self.labels.flatten())[0]
-        uniq_labels = list(set(labels))
-
-        x, y = self.objects.objects[:, features[0]],\
-            self.objects.objects[:, features[1]]
-
-        from mpl_toolkits.axes_grid1 import make_axes_locatable
-        # the scatter plot:
-        axScatter = plt.subplot(111)
-        # axScatter.set_aspect(1.) # square image
-
-        for index, uniq_label in enumerate(uniq_labels):
-            plt.plot(x[labels == uniq_label], y[labels == uniq_label],
-                '%so' % COLORS[index])
-
-        # define picture size
-        xlim, ylim = plt.xlim(), plt.ylim()
-
-        # create new axes on the right and on the top of the current axes
-        # The first argument of the new_vertical(new_horizontal) method is
-        # the height (width) of the axes to be created in inches.
-        divider = make_axes_locatable(axScatter)
-        axHistx = divider.append_axes("top", 1.2, pad=0.1, sharex=axScatter)
-        axHisty = divider.append_axes("right", 1.2, pad=0.1, sharey=axScatter)
-
-        # make some axis - labels invisible
-        plt.setp(axHistx.get_xticklabels() + axHisty.get_yticklabels(),
-            visible=False)
-
-        # now determine nice limits by hand:
-        binwidth = 0.5
-        xymax = np.max( [np.max(np.fabs(x)), np.max(np.fabs(y))] )
-        lim = ( int(xymax/binwidth) + 1) * binwidth
-
-        bins = np.arange(-lim, lim + binwidth, binwidth)
-
-        x_bar, y_bar, color = [], [], []
-        for index, uniq_label in enumerate(uniq_labels):
-            x_bar.append(x[labels == uniq_label])
-            y_bar.append(y[labels == uniq_label])
-            color.append(COLORS[index])
-
-        axHistx.hist(x_bar, bins=bins, color=color)
-        axHisty.hist(y_bar, bins=bins, color=color, orientation='horizontal')
-
-        # plot according to size
-        width = xlim[1] - xlim[0]
-        height = ylim[1] - ylim[0]
-        # add +- 5% to border
-        xlim = (xlim[0] - width / 20, xlim[1] + width / 20)
-        ylim = (ylim[0] - height / 20, ylim[1] + height / 20)
-
-        axScatter.set_xlim(xlim)
-        axScatter.set_ylim(ylim)
-
-        # plt.draw()
-        plt.show()
-
-Field = namedtuple('Field', 'name type')
+Feature = namedtuple('Feature', 'type')
 
 class Data(object):
     """
-    fields = list<field>
-    field = (field name, field type), namedtuple
+    General data representation: objects, labels and features
+    objects: ObjectFeatureMatrix
+    labels: None of ObjectFeatureMatrix
+    features: list<Feature>
     """
-    def __init__(self, objects, labels=None, fields=None):
-        self.objects = np.matrix(objects)
-        self.labels = np.matrix(labels) if labels is not None else np.zeros((objects.shape[0], 1))
-        self.fields = fields
+    def __init__(self, objects, labels=None, features=None):
+        self.x = ObjectFeatureMatrix(objects)
+        self.features = features
 
-        assert self.labels.shape[1] == 1, "size of labels must be (m x 1)"
+        if self.features is not None and len(features) != self.x.nfeatures:
+            msg = "Number of features and columns in objects should be equal"
+            raise ValueError(msg)
 
-    def get_number_objects(self):
-        return self.objects.shape[0]
-    nobjects = property(get_number_objects, doc="return number of objects")
+        if labels is not None:
+            self.y = np.matrix(labels)
+        else:
+            self.y = np.matrix([[None]] * len(features))
 
-    def get_number_features(self):
-        return self.objects.shape[1]
-    nfeatures = property(get_number_features, doc="return number of features")
+    @property
+    def nfeatures(self):
+        return len(self.features)
 
-    def get_vif(self):
-        assert self.nfeatures > 1
 
-        def _regression_residuals(x, y):
-            """
-            Calculate regression residuals:
-            y - x * (x' * x)^(-1) * x' * y;
-            Input:
-                x - array(l, n)
-                y - array(l, 1)
-            Output:
-                residuals y - x*w - array(l, 1)
-            """
-            return np.asarray(y - x * (x.T * x)**(-1) * x.T * y)
 
-        vif = [0] * self.nfeatures
-        for i in range(self.nfeatures):
-            rows = range(i) + range(i + 1, self.nfeatures)
-            vif[i] = float(sum(
-                    np.asarray(self.objects[:, i] -
-                               np.mean(self.objects[:, i])) ** 2) /
-                    sum(_regression_residuals(self.objects[:, rows],
-                                              self.objects[:, i]) ** 2)
-                           )
-        return vif
+
+class BigData(object):
+    """
+    Big data representation. If it is not possible to load data into memory
+    one can use iterator technique. It allows to calculate base statistics
+    on data and later use chuncks precessing
+    """
+    pass
+
 
 
 class DataReader(object):
@@ -289,6 +127,61 @@ class DataReader(object):
         return data
 
 
+class DataStorage(object):
+
+
+
+    # def __init__(self, data, dtype='None', copy='True', features=None):
+    #     super(self.__class__, self).__init__(data, dtype=dtype, copy=copy)
+    #     self.features = features
+    #     if self.features is not None and len(self.features) != self.nfeatures:
+    #         msg = "Number of features and matrix columns should be the same"
+    #         raise ValueError(msg)
+
+
+
+    def __init__(self):
+        self.__fields = None
+
+    def load(self, dataset_name):
+        """
+        Load dataset for given name
+        """
+        if not dataset_name in settings.DATA_SETS:
+            raise ValueError("Dataset does not exist")
+        else:
+            self.__datastream = tempfile.TemporaryFile()
+            dataset_url = "%s/%s.tsv.gz" % (settings.DATA_HOST, dataset_name)
+            response = urllib2.urlopen(dataset_url, timeout=10)
+            for line in gzip.GzipFile(fileobj=StringIO(response.read())):
+                self.__datastream.write(line)
+
+    @property
+    def lines(self):
+        self.__datastream.seek(0)
+        for line in self.__datastream:
+            yield line
+
+    def __get_fields(self):
+        header = itertools.islice(self.lines, 1).next()
+        heared_prefix = "# "
+        if not header.startswith(heared_prefix):
+            msg = 'Bad header format. Should starts from "%s"' % heared_prefix
+            raise ValueError(msg)
+        else:
+            header = header[len(heared_prefix):].split('#', 1)[0].rstrip()
+            header = header.replace(",", "\t").replace(";", "\t")
+
+        fields = [Field(field.split(':')[0], field.split(':')[1])
+                  for field in header.split("\t")]
+        return fields
+
+    @property
+    def fields(self):
+        if self.__fields is None:
+            self.__fields = self.__get_fields()
+        return self.__fields
+
+
 if __name__ == "__main__":
-    import doctest
-    doctest.testfile("%s.test" % __file__.split(".", 1)[0])
+    pass
