f3bc5e6ff319c7b79d0e1cf705cd3e13b84329ba
Kirill Pavlov <kirill.pavlov@phystech.edu>
2012-12-29 17:43:11 +0800

    add nose2 to requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ setup(
     name="pmll",
     version="0.1",
     packages=find_packages(),
+    test_suite="nose2.collector.collector",
 
     # metadata for upload to PyPI
     author="Kirill Pavlov",
