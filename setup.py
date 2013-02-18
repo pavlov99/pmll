da12ce232bd72eb91014969bcecff84090e7b9b3
Kirill Pavlov <kp@multichannel.net>
2013-02-19 00:41:44 +0800

    update package info, add versiontool

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from setuptools import setup, find_packages
 setup(
     name="pmll",
-    version="0.1",
+    version=":versiontools:pmll:",
     packages=find_packages(),
     test_suite="nose2.collector.collector",
+    setup_requires=[
+        'versiontools >= 1.8',
+    ],
 
     # metadata for upload to PyPI
     author="Kirill Pavlov",
     author_email="kirill.pavlov@phystech.edu",
+    url="pmll.kirillpavlov.com",
     description="Python machine learning library",
+    keywords="data mining",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+    ],
     license="MIT",
 )
