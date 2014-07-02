450c6ab734d36febcb20a445ad318483f851f8a0
Kirill Pavlov <kirill.pavlov@phystech.edu>
2014-07-02 09:01:14 +0000

    add classification quality measures

--- pmll/metrics/base.py
+++ pmll/metrics/base.py
@@ -66,6 +66,96 @@ class QualityMeasurerLinear(object):
         self.sae += abs(predicted - actual)
 
 
+class QualityMeasurerNominal(object):
+    def __init__(self):
+        self.tp = 0
+        self.tn = 0
+        self.fp = 0
+        self.fn = 0
+
+    @property
+    def tpr(self):
+        """ True Positive Rate.
+
+        Also known as sensitivity or recall.
+        Ratio of correct objects identified as correct.
+
+        """
+        return float(self.tp) / (self.tp + self.fn)
+
+    @property
+    def tnr(self):
+        """ True Negative Rate.
+
+        Also known as specificity.
+
+        """
+        return float(self.tn) / (self.fp + self.tn)
+
+    @property
+    def specificity(self):
+        return self.tnr
+
+    @property
+    def sensitivity(self):
+        return self.tpr
+
+    @property
+    def recall(self):
+        return self.tpr
+
+    @property
+    def precision(self):
+        return float(self.tp) / (self.tp + self.fp)
+
+    @property
+    def accuracy(self):
+        return float(self.tp + self.tn) /\
+            (self.tp + self.tn + self.fp + self.fn)
+
+    def f(self, beta):
+        """ F measure.
+
+        Measure of a test's accuracy. It considers both the precision and the
+        recall of the test to compute the score.
+
+        F(b) = (1 + b^2) * precision * recall / (b^2 * precision + recall)
+
+        """
+        b = beta ** 2
+        return float(1 + b) * self.tp / (
+            (1 + b) * self.tp + b * self.fn + self.fp)
+
+    @property
+    def f1(self):
+        return self.f(1)
+
+    @property
+    def mcc(self):
+        """ Matthews correlation coefficient.
+
+        The Matthews correlation coefficient is used in machine learning as
+        a measure of the quality of binary (two-class) classifications. It
+        takes into account true and false positives and negatives and is
+        generally regarded as a balanced measure which can be used even if the
+        classes are of very different sizes. The MCC is in essence a
+        correlation coefficient between the observed and predicted binary
+        classifications.
+
+        """
+        return float(self.tp * self.tn - self.fp * self.fn) / math.sqrt(
+            (self.tp + self.fp) * (self.tp + self.fn) * (self.tn + self.fp) *
+            (self.tn + self.fn))
+
+    def append(self, predicted, actual):
+        if predicted == actual:
+            self.tp += int(bool(predicted))
+            self.tn += int(not bool(predicted))
+        else:
+            self.fp += int(bool(predicted))
+            self.fn += int(not bool(predicted))
+
+
 def measure(data_predicted, data_actual, feature=None):
     predicted = data_predicted[:, data_predicted.features.index(feature)]
     actual = data_actual[:, data_actual.features.index(feature)]
